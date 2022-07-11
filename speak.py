6ed8082544937b1f4b8e9537a559ebc1548cf92b
Norm×Alpha.vg <87635928+NormVg@users.noreply.github.com>
2022-07-11 13:24:03 +0530

    Update speak.py

--- speak.py
+++ speak.py
@@ -1,11 +1,11 @@
-import os 
+import os ,time
 from gtts import gTTS
 from termcolor import cprint
 
 def say(text):
-    cprint(text,"red")
     speak=gTTS(text,lang="en")
     speak.save("tts.mp3")
     os.system(f"termux-media-player play tts.mp3")
+    cprint(text,"cyan")
     os.remove("tts.mp3")
-    
+    time.sleep(.5)
